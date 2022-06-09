4bcb6e98917300f79d1fb4c1dbc48caded27478e
masax <masax.eth@gmail.com>
2022-06-09 12:11:26 +0700

    1)Add trigger to a replied message. 2)Add reaction to the triggered message

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import DiscordJS, { Intents, TextChannel, MessageEmbed, Snowflake, ColorResolvable, }  from 'discord.js'
+import DiscordJS, { Intents, TextChannel, MessageEmbed, Snowflake, Message }  from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -6,12 +6,15 @@ const CHANNEL_ID = process.env.REMINDER_CHANNEL_ID as Snowflake
 const TRIGGER_MESSAGE = process.env.TRIGGER_MESSAGE as string
 const REMINDER_TO_MENTION_ID = process.env.REMINDER_TO_MENTION_ID as Snowflake
 const MESSAGE_LINE = process.env.MESSAGE_LINE as string
+const REACT_EMOJI = process.env.REACT_EMOJI as string
+
 
 
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ],
 })
 
@@ -21,16 +24,37 @@ client.on('ready', () => {
 
 client.on('messageCreate', (message) => {
 
-    if (message.author.bot) return;
+    // if it's from a bot
+    if (message.author.bot) return
 
-    // only when mentioned or includes texts
-    if(REMINDER_TO_MENTION_ID !== "" && (message.content.includes(`<@${REMINDER_TO_MENTION_ID}>`)) || 
-    (TRIGGER_MESSAGE !== "" && message.content.includes(TRIGGER_MESSAGE))) {
+    // If it's from the target person
+    if (message.author.id === REMINDER_TO_MENTION_ID) {
+        return
+    }
+
+    // check if it's a reply to another message
+    let repliedMessage = null
+    if (message.reference !== null) {
+        let repliedMessageId = message.reference.messageId as string
+        let repliedChannelId = message.reference.channelId as string
+        let repliedGuildId = message.reference.guildId as string
+
+        // if replied message's author is the target person
+        let repliedChannel = client.guilds.cache.get(repliedGuildId)?.channels.cache.get(repliedChannelId) as TextChannel
+        repliedMessage = repliedChannel.messages.cache.get(repliedMessageId) as Message
+    }
+
+    if (
+        // only when mentioned or includes texts
+        checkMessage(message) || 
+        // or repliedMessage is from the target or repliedMessage contains the condition
+        (repliedMessage != null && (repliedMessage.author.id === REMINDER_TO_MENTION_ID || checkMessage(repliedMessage)))) {
         let author = message.author;
         let username = author.username
         let avatorDisplayUrl = author.displayAvatarURL({ dynamic: true})
         let link = message.url;
         let channel = client.channels.cache.get(CHANNEL_ID) as TextChannel
+
 
         const exampleEmbed = new MessageEmbed()
             .setColor('#674cf5')
@@ -43,13 +67,27 @@ client.on('messageCreate', (message) => {
             })
             .setDescription(message.content)
             .setTimestamp()
-        channel.send(
-            { 
-                content: `<@${REMINDER_TO_MENTION_ID}> ${username} ${MESSAGE_LINE}`,
-                embeds: [exampleEmbed] 
-            }
-        );
+        
+        try {
+            channel.send(
+                { 
+                    content: `<@${REMINDER_TO_MENTION_ID}> ${username} ${MESSAGE_LINE}`,
+                    embeds: [exampleEmbed] 
+                }
+            );
+        } finally {
+            // react with emoji when the message is sent successfuly
+            message.react(REACT_EMOJI)
+        }
     }
 })
+
+
+function checkMessage (message: Message): boolean {
+    return REMINDER_TO_MENTION_ID !== "" && (message.content.includes(`<@${REMINDER_TO_MENTION_ID}>`)) || 
+    (TRIGGER_MESSAGE !== "" && message.content.includes(TRIGGER_MESSAGE))
+}
+
+
 
 client.login(process.env.TOKEN)
